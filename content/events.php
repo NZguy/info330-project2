e02167ce37bec12cc408e82a869d53a2d92d55a1
Kodlee Yin <kodlee@kodleeshare.net>
2016-05-06 16:23:08 -0700

    Wrote example autogeneration of html

--- content/events.php
+++ content/events.php
@@ -2,19 +2,24 @@
 require_once PHP_ROOT . '/i330p2/Setup.php';
 use i330p2\template\StaticPage;
 
-$body = <<<HTML
-
-<?php
-for($i = 1; $i <= 6; $i++){
-?>
-    <div class="event"></div>
-<?php
+// Make the events div first and store the HTML in a variable
+$eventHtml = "";
+for ($i = 1; $i <= 6; $i++) {
+	$eventHtml += '<div class="event"></div>';
 }
-?>
 
+// Php automatically spits out what's in the variable when it's between quotes
+// Remember to treat what's in between <<<HTML and HTML; like quotes.
+// This is the same as saying
+// $body = "This is text before \r\n $eventHtml \r\n this is text after";
+// Remember, \r\n means "newline"
+$body = <<<HTML
+	This is text before
+	$eventHtml
+	This is text after
 HTML;
 
 StaticPage::createContent()
-    ->with(StaticPage::FIELD_TITLE, "Home")
+    ->with(StaticPage::FIELD_TITLE, "Events")
     ->with(StaticPage::FIELD_BODY, $body)
     ->render();
