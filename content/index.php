a496c50010797722ef68b2bb79a8a2a1a51408ce
Duncan Andrew <duncanandrew@live.com>
2016-05-06 14:55:45 -0700

    created head div

--- content/index.php
+++ content/index.php
@@ -3,16 +3,17 @@ require_once PHP_ROOT . '/i330p2/Setup.php';
 use i330p2\template\StaticPage;
 
 $body = <<<HTML
-
-<button id="hamburger1">&#9776</button>
-<div id="nav" class="close">
-	<button id="hamburger2">&#9776</button>
-	<ul>
-		<li>Profile</li>
-		<li>Events</li>
-		<li>BoatSpace</li>
-		<li>Help</li>
-	</ul>
+<div id="header">
+	<button id="hamburger1">&#9776</button>
+	<div id="nav" class="close">
+		<button id="hamburger2">&#9776</button>
+		<ul>
+			<li>Profile</li>
+			<li>Events</li>
+			<li>BoatSpace</li>
+			<li>Help</li>
+		</ul>
+	</div>
 </div>
 
 HTML;
