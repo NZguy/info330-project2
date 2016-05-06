8de6e0dbe6d951f2209697ac6ec1d8c915a14e10
Duncan Andrew <duncanandrew@live.com>
2016-05-06 12:05:41 -0700

    fledgeling hamburger menu added

--- content/index.php
+++ content/index.php
@@ -3,7 +3,16 @@ require_once PHP_ROOT . '/i330p2/Setup.php';
 use i330p2\template\StaticPage;
 
 $body = <<<HTML
-hi there
+
+<div id="nav">
+	<ul>
+		<li>Profile</li>
+		<li>Events</li>
+		<li>BoatSpace</li>
+		<li>Help</li>
+	</ul>
+</div>
+
 HTML;
 
 StaticPage::createContent()
