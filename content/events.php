9eabe9e93f7cbfa36c78a4af3dea729451d15216
Duncan Andrew <duncanandrew@live.com>
2016-05-06 15:43:58 -0700

    created new pages

---
+++ content/events.php
@@ -0,0 +1,12 @@
+<?php
+require_once PHP_ROOT . '/i330p2/Setup.php';
+use i330p2\template\StaticPage;
+
+$body = <<<HTML
+
+HTML;
+
+StaticPage::createContent()
+    ->with(StaticPage::FIELD_TITLE, "Home")
+    ->with(StaticPage::FIELD_BODY, $body)
+    ->render();
